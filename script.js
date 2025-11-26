31552c8071aa9e99bd435592747d16754be27f5c
Sergio Armando Acevedo Chacón <sergioacevedo@MacBook-Pro-de-Sergio-2.local>
2025-11-25 17:05:18 -0700

    feat: Cambiar fondo con Js en feature-js

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ document.getElementById('btn').addEventListener('click', () => {
     alert('Hola desde script.js');
 });
 
+document.body.style.backgroundColor = '#fffacd'; // cambio intencional en JS
+
